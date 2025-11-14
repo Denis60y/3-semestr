ae5c88fd6325ac76f81a6ae64a9fb352a185df79
pitt0n <matweew20006@gmail.com>
2025-11-14 11:03:05 +0300

    Дописал третий конструктор и добавил новое поле quantity для класса

--- High_Level_Programming/Lab_4/Item.java
+++ High_Level_Programming/Lab_4/Item.java
@@ -4,18 +4,34 @@ class Item {
     private String name;        
     private String id;          
     private int maxStackSize;
+    private int quantity;
  
+
+    public Item(String name, String id, int maxStackSize, int quantity){
+        this.name = name;
+        this.id = id;
+        this.maxStackSize = maxStackSize;
+        if (quantity > maxStackSize){
+            this.quantity = 0;
+            System.out.println("Количество прдмета в ячейве не может превышать максимальное возможное количество");
+        }
+        else{
+            this.quantity = quantity;
+        }
+    }
 
     public Item(String name, String id, int maxStackSize){
         this.name = name;
         this.id = id;
         this.maxStackSize = maxStackSize;
+        this.quantity = maxStackSize;
     }
-
+    
     public Item(String name, String id){
         this.name = name;
         this.id = id;
         this.maxStackSize = 64;
+        this.quantity = this.maxStackSize;
     }
 
     public void setName(String name){this.name = name;}
@@ -27,8 +43,11 @@ class Item {
     public void setMaxStackSize(int maxStackSize){this.maxStackSize = maxStackSize;}
     public int getMaxStackSize(){return maxStackSize;}
 
+    public void setQuantity(int quantity){this.quantity = quantity;}
+    public int getQuantity(){return quantity;}
+
     public void getInfo(){
-        System.out.printf("Название предмета: %s\nID предмета: %s\nМаксимальное количество в слоте: %d\n", name, id, maxStackSize);
+        System.out.printf("Название предмета: %s\nID предмета: %s\nМаксимальное количество в слоте: %d\nКоличество предметов в яцейке: %d\n", name, id, maxStackSize, quantity);
     }
 }
 
