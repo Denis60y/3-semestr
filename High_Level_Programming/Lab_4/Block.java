032605a68545db17b3fc4570e16593112fff4382
pitt0n <matweew20006@gmail.com>
2025-11-16 16:26:16 +0300

    Форматирование кода

--- High_Level_Programming/Lab_4/Block.java
+++ High_Level_Programming/Lab_4/Block.java
@@ -1,30 +1,37 @@
 package High_Level_Programming.Lab_4;
 
-class Block extends Item{
+class Block extends Item {
     private boolean full;
-    
 
-    public Block(String name, String id, int maxStackSize, int quantitym, boolean full){
-        super(name, id, maxStackSize, quantitym);
+    public Block(String name, String id, int maxStackSize, int quantity, boolean full) {
+        super(name, id, maxStackSize, quantity);
         this.full = full;
     }
 
-    public Block(String name, String id, int maxStackSize, boolean full){
+    public Block(String name, String id, int maxStackSize, boolean full) {
         super(name, id, maxStackSize);
         this.full = full;
     }
 
-    public Block(String name, String id, boolean full){
+    public Block(String name, String id, boolean full) {
         super(name, id);
         this.full = full;
     }
-    
-    public void setFull(boolean full){this.full = full;}
-    public boolean getFull(){return full;}
 
-    public void getInfo(){
-        System.out.printf("Название предмета: %s\nID предмета: %s\nМаксимальное количество в слоте: %d\nКоличество предметов в яцейке: %d\n" + //
-                        "Полный блок: %b\n", getName(), getId(), getMaxStackSize(), getQuantity(), full);
+    public void setFull(boolean full) {
+        this.full = full;
+    }
+
+    public boolean getFull() {
+        return full;
+    }
+
+    public void getInfo() {
+        System.out.printf(
+                "Название предмета: %s\nID предмета: %s\nМаксимальное количество в слоте: %d\nКоличество предметов в яцейке: %d\n"
+                        + //
+                        "Полный блок: %b\n",
+                getName(), getId(), getMaxStackSize(), getQuantity(), full);
     }
 
 }
