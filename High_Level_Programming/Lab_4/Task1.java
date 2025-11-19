a65906b0b834e4aab3822e1fb9016bb87e1484e1
Pitt0n <matweew20006@gmail.com>
2025-11-19 21:54:51 +0300

    Вызов и текст программы

--- High_Level_Programming/Lab_4/Task1.java
+++ High_Level_Programming/Lab_4/Task1.java
@@ -6,15 +6,17 @@ package High_Level_Programming.Lab_4;
  * 
  * Автор: @Pitt0n
  * Дата создания: 08.11.2025
- * Версия: 0.0.7
+ * Версия: 0.0.8
  */
 
 
 public class Task1 {
     public static void main(String[] args) {
-        Food item1 = new Food("Яблоко", "apple", 64, 1, 5, 35);
-        Player Steve = new Player(20, 10, false);
+        Food Apple = new Food("Яблоко", "apple", 64, 1, 5, 35);
+        Player Steve = new Player("Steve",20, 10, false);
         Block block = new Block("Grass", "grass", true, false, false);
-        block.getInfo();
+        Steve.addItem(block);
+        Steve.addItem(Apple);
+        Steve.getInfoItems();
     }
 }
