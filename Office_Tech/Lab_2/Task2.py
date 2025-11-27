3c8331866387f0e8b69d45e270feeceb5e3abd1c
Pitt0n <matweew20006@gmail.com>
2025-11-27 19:45:23 +0300

    Доработка

--- Office_Tech/Lab_2/Task2.py
+++ Office_Tech/Lab_2/Task2.py
@@ -9,11 +9,25 @@ def clear_cell_paragraphs(cell):
 
 doc = docx.Document()
 
+style = doc.styles['Normal']
+style.font.name = 'Times New Roman'
+
 table = doc.add_table(6, 2)
 table.style = 'Table Grid'
 
 for i in range(5):
     table.cell(i+1,0).merge(table.cell(i+1,1))
+
+# def new_parafraph(cell, text, fontSize, bolt, italic, underline):
+#     head = cell.add_paragraph()
+#     p_fmt = head.paragraph_format
+#     p_fmt.space_after = Pt(10)
+#     add = head.add_run(text)
+#     add.font.name = 'Times New Roman'
+#     add.font.size = Pt(fontSize)
+#     add.bold = bolt
+#     add.font.italic = italic
+#     add.underline = underline
 
 #ячейка номер 1
 
@@ -25,7 +39,6 @@ head = cell.add_paragraph()
 p_fmt = head.paragraph_format
 p_fmt.space_after = Pt(10)
 add = head.add_run("Дата поступления")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 
 void = cell.add_paragraph()
@@ -64,8 +77,9 @@ cell = table.cell(1, 0)
 clear_cell_paragraphs(cell)
 
 head = cell.add_paragraph()
+p_fmt = head.paragraph_format
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 add = head.add_run("Согласие на указание сведений об авторе в заявлении на государственную регистрацию программы для ЭВМ или базы данных")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(13)
 add.bold = True
 
@@ -76,7 +90,6 @@ cell = table.cell(2, 0)
 clear_cell_paragraphs(cell)
 head = cell.add_paragraph()
 add = head.add_run("Заявка  №  ____________________________________________________________")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 
 head = cell.add_paragraph()
@@ -84,7 +97,6 @@ p_fmt = head.paragraph_format
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 p_fmt.space_after = Pt(10)
 add = head.add_run("(указывается при наличии регистрационного номера заявки)")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(7)
 add.font.italic = True
 
@@ -92,26 +104,20 @@ dop = cell.add_paragraph()
 p_fmt = head.paragraph_format
 p_fmt.space_after = Pt(10)
 add = dop.add_run("на государственную регистрацию: ")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 add = dop.add_run("☒ ")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(10)
-add = dop.add_run("Программы для ЭВМ □ Базы данных, государственная регистрация которой осуществляется в соответствии с пунктом 4 статьи 1259 Кодекса □ Базы данных, государственная регистрация которой осуществляется в соответствии с пунктом 3 статьи 1334 Кодекса ")
-add.font.name = 'Times New Roman'
+add = dop.add_run("Программы для ЭВМ ☐ Базы данных, государственная регистрация которой осуществляется в соответствии с пунктом 4 статьи 1259 Кодекса ☐ Базы данных, государственная регистрация которой осуществляется в соответствии с пунктом 3 статьи 1334 Кодекса ")
 add.font.size = Pt(9)
 add.bold = True
 add = dop.add_run("(Отметить знаком «Х» вид результата интеллектуальной деятельности)")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(9)
 add.font.italic = True
 
 dop = cell.add_paragraph()
 add = dop.add_run("Название:")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 add = dop.add_run(" «Название программы ЭВМ»")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 add.underline = True
 
@@ -119,7 +125,6 @@ dop = cell.add_paragraph()
 p_fmt = dop.paragraph_format
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 add = dop.add_run("(указывается в  соответствии с графой 1 заявления о государственной регистрации программы для ЭВМ или базы данных)")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(8)
 add.font.italic = True
 
@@ -131,29 +136,96 @@ clear_cell_paragraphs(cell)
 
 head = cell.add_paragraph()
 add = head.add_run("Правообладатель (и) (Заявитель)(и) ")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 add = head.add_run("(указываются фамилия, имя, отчество (последнее – при наличии), место жительства физического лица, наименование, место нахождения, основной государственный регистрационный номер (ОГРН)  и идентификационный номер налогоплательщика (ИНН) юридического лица) ")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(9)
 add.font.italic = True
 
 head = cell.add_paragraph()
 add = head.add_run("Физ. Лицо -  указываются фамилия, имя, отчество (последнее – при наличии), место жительства")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 add.underline = True
 
 dop = cell.add_paragraph()
 add = dop.add_run("Подтверждаю согласие на указание обо мне, как авторе, следующих сведений в графе 7А заявления на государственную регистрацию данной программы для ЭВМ или базы данных.")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(11)
 add.font.italic = True
 add.bold = True
 
 dop = cell.add_paragraph()
 add = dop.add_run("7А. СВЕДЕНИЯ ОБ АВТОРЕ:")
-add.font.name = 'Times New Roman'
+add.font.size = Pt(10)
+add.font.italic = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Фамилия имя отчество:")
+add.font.size = Pt(10)
+add.font.italic = True
+add = dop.add_run("  Фамилия Имя Отчество")
+add.font.size = Pt(12)
+add.bold = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Фамилия имя отчество:")
+add.font.size = Pt(10)
+add.font.italic = True
+add = dop.add_run("  11_   ")
+add.font.size = Pt(10)
+add.bold = True
+add.underline = True
+add = dop.add_run("месяц: ")
+add.font.size = Pt(10)
+add.font.italic = True
+add = dop.add_run("  11   ")
+add.font.size = Pt(10)
+add.bold = True
+add.underline = True
+add = dop.add_run("год: ")
+add.font.size = Pt(10)
+add.font.italic = True
+add = dop.add_run("  2001   ")
+add.font.size = Pt(10)
+add.bold = True
+add.underline = True
+add = dop.add_run("Гражданство:")
+add.font.size = Pt(10)
+add.font.italic = True
+add = dop.add_run("_Гражданство_")
+add.font.size = Pt(10)
+add.bold = True
+add.underline = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Автор согласен с обработкой указанных персональных данных, необходимой для исполнения полномочий федеральных органов исполнительной власти, участвующих в предоставлении государственных услуг, предусмотренных Федеральным законом от 27 июля 2010 года № 210-ФЗ «Об организации предоставления государственных и муниципальных услуг», включая регистрацию субъекта персональных данных на едином портале государственных и муниципальных услуг и (или) региональных порталах государственных и муниципальных услуг и в течение срока действия исключительного права на регистрируемый объект.")
+add.font.size = Pt(10)
+add.bold = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Место постоянного жительства, включая указание страны:")
+add.font.size = Pt(10)
+add.font.italic = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Место постоянного жительства")
+add.font.size = Pt(12)
+add.bold = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Краткое описание творческого вклада автора при создании регистрируемой программы для ЭВМ или базы данных:")
+add.font.size = Pt(10)
+add.font.italic = True
+
+dop = cell.add_paragraph()
+add = dop.add_run("Написание исходного текста программы – 0 %")
+add.font.size = Pt(11)
+add.bold = True
+
+void = cell.add_paragraph()
+add = void.add_run("")
+add.font.size = Pt(10)
+
+dop = cell.add_paragraph()
+add = dop.add_run("При  публикации сведений о государственной регистрации программы для ЭВМ или базы данных автор просит: (отметить [X])")
 add.font.size = Pt(10)
 add.font.italic = True
 
@@ -165,17 +237,14 @@ clear_cell_paragraphs(cell)
 
 head = cell.add_paragraph()
 add = head.add_run("Подпись автора:                                                                     И.О. Фамилия")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 
 void = cell.add_paragraph()
 add = void.add_run("")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(10)
 
 dop = cell.add_paragraph()
 add = dop.add_run("(подпись должна быть расшифрована)")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(9)
 add.font.italic = True
 
@@ -187,17 +256,14 @@ clear_cell_paragraphs(cell)
 
 head = cell.add_paragraph()
 add = head.add_run("Подпись(и) правообладателя(ей) или его (их) представителя(ей)")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 
 void = cell.add_paragraph()
 add = void.add_run("")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(12)
 
 dop = cell.add_paragraph()
 add = dop.add_run("_________Должность____________________________И.О. Фамилия_______________")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(11)
 add.underline = True
 
@@ -205,7 +271,6 @@ dop = cell.add_paragraph()
 p_fmt = dop.paragraph_format
 p_fmt.space_after = Pt(10)
 add = dop.add_run(" (от имени юридического лица заявление подписывается руководителем организации или иным лицом, уполномоченным на это в установленном законодательством Российской Федерации порядке, с указанием его должности, подпись удостоверяется печатью юридического лица при наличии печати. Подпись любого лица должна быть расшифрована с указанием фамилии и инициалов и даты подписания заявления)")
-add.font.name = 'Times New Roman'
 add.font.size = Pt(7)
 add.font.italic = True
 
