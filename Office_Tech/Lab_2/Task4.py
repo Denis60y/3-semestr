ed8185e7014f590e10a3f70d36cb57dd09e3e30b
Pitt0n <matweew20006@gmail.com>
2025-12-02 19:43:39 +0300

    доработка файла

--- Office_Tech/Lab_2/Task4.py
+++ Office_Tech/Lab_2/Task4.py
@@ -24,10 +24,96 @@ p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 add = text.add_run("Полное название организации")
 add.font.size = Pt(14)
 
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("\n\n\n\n\n")
+add.font.size = Pt(14)
 
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.RIGHT
+add = text.add_run("Название кафедры")
+add.font.size = Pt(14)
 
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("\n\n\n\n\n\n\n\n")
+add.font.size = Pt(14)
 
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+add = text.add_run("Отчет по лабораторной работе №#")
+add.font.size = Pt(24)
 
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+add = text.add_run("по дисциплине «Название дисциплины»")
+add.font.size = Pt(16)
 
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("\n")
+add.font.size = Pt(16)
+add = text.add_run("\n\n")
+add.font.size = Pt(14)
+add = text.add_run("\n\n\n\n")
+add.font.size = Pt(18)
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("                                                           ")
+add.font.size = Pt(18)
+add = text.add_run("Выполнил: студент группы Номер ")
+add.font.size = Pt(14)
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("                                                                                                Фамилия И.О.")
+add.font.size = Pt(14)
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("                                                                            Проверил:  Фамилия И.О.")
+add.font.size = Pt(14)
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.left_indent = Cm(-0.63)
+add = text.add_run("\n\n\n\n")
+add.font.size = Pt(12)
+add = text.add_run("\n\n\n\n\n")
+add.font.size = Pt(14)
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+add = text.add_run("Тамбов 2001")
+add.font.size = Pt(14)
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.first_line_indent = Cm(1.25)
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+add = text.add_run("Цель работы:")
+add.font.size = Pt(14)
+add.bold = True
+
+text = doc.add_paragraph()
+p_fmt = text.paragraph_format
+p_fmt.first_line_indent = Cm(1.25)
+p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+add = text.add_run("Зачем именно нужна данная работа. Что она позволяет освоить")
+add.font.size = Pt(14)
 
 doc.save("resources/Структура отчета.docx")
